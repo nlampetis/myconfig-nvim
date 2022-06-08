519da518ed2607b7e6e09dc593881b366c6c7d44
nlampetis <65315958+nlampetis@users.noreply.github.com>
2022-06-08 17:32:57 +0300

    added gitignore

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "C:\\Users\\TET_VO~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\share\\lua\\5.1\\?.lua;C:\\Users\\TET_VO~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\share\\lua\\5.1\\?\\init.lua;C:\\Users\\TET_VO~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\luarocks\\rocks-5.1\\?.lua;C:\\Users\\TET_VO~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\luarocks\\rocks-5.1\\?\\init.lua"
-local install_cpath_pattern = "C:\\Users\\TET_VO~1\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\lua\\5.1\\?.so"
+local package_path_str = "C:\\Users\\Nikos\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\share\\lua\\5.1\\?.lua;C:\\Users\\Nikos\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\share\\lua\\5.1\\?\\init.lua;C:\\Users\\Nikos\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\luarocks\\rocks-5.1\\?.lua;C:\\Users\\Nikos\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\luarocks\\rocks-5.1\\?\\init.lua"
+local install_cpath_pattern = "C:\\Users\\Nikos\\AppData\\Local\\Temp\\nvim\\packer_hererocks\\2.1.0-beta3\\lib\\lua\\5.1\\?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -71,175 +71,175 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\LuaSnip",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   airline = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\airline",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\airline",
     url = "https://github.com/vim-airline/vim-airline"
   },
   ["airline-themes"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\airline-themes",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\airline-themes",
     url = "https://github.com/vim-airline/vim-airline-themes"
   },
   ["auto-pairs"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\auto-pairs",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\auto-pairs",
     url = "https://github.com/jiangmiao/auto-pairs"
   },
   ["barbar.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\barbar.nvim",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\barbar.nvim",
     url = "https://github.com/romgrk/barbar.nvim"
   },
   ["clang-tidy.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\clang-tidy.nvim",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\clang-tidy.nvim",
     url = "https://github.com/emilienlemaire/clang-tidy.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-buffer",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-cmdline"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-cmdline",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-cmdline",
     url = "https://github.com/hrsh7th/cmp-cmdline"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-nvim-lsp",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-path",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp_luasnip",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   dracula = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\dracula",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\dracula",
     url = "https://github.com/dracula/vim"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\friendly-snippets",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   gruvbox = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\gruvbox",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\gruvbox",
     url = "https://github.com/morhetz/gruvbox"
   },
   ["indent-blankline.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\indent-blankline.nvim",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
   ["lspkind.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lspkind.nvim",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lspkind.nvim",
     url = "https://github.com/onsails/lspkind.nvim"
   },
   nerdcommenter = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nerdcommenter",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nerdcommenter",
     url = "https://github.com/preservim/nerdcommenter"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-cmp",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-keymap-amend"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-keymap-amend",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-keymap-amend",
     url = "https://github.com/anuvyklack/nvim-keymap-amend"
   },
   ["nvim-lsp-installer"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-lsp-installer",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-lsp-installer",
     url = "https://github.com/williamboman/nvim-lsp-installer"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-lspconfig",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-treesitter",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-web-devicons",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   nvimtree = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvimtree",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvimtree",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   onedark = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\onedark",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\onedark",
     url = "https://github.com/navarasu/onedark.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\packer.nvim",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\plenary.nvim",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["pretty-fold.nvim"] = {
     config = { "\27LJ\2\na\0\0\3\0\4\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\0016\0\0\0'\2\3\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\24pretty-fold.preview\nsetup\16pretty-fold\frequire\0" },
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\pretty-fold.nvim",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\pretty-fold.nvim",
     url = "https://github.com/anuvyklack/pretty-fold.nvim"
   },
   tagbar = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\tagbar",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\tagbar",
     url = "https://github.com/preservim/tagbar"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\telescope.nvim",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-doge"] = {
     config = { "\27LJ\2\nA\0\0\2\0\4\0\0056\0\0\0009\0\1\0'\1\3\0=\1\2\0K\0\1\0\vgoogle\29doge_doc_standard_python\6g\bvim\0" },
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-doge",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-doge",
     url = "https://github.com/kkoomen/vim-doge"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-fugitive",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-startify"] = {
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-startify",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-startify",
     url = "https://github.com/mhinz/vim-startify"
   },
   whichkey = {
     config = { "\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\0" },
     loaded = true,
-    path = "C:\\Users\\tet_voithos_4\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\whichkey",
+    path = "C:\\Users\\Nikos\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\whichkey",
     url = "https://github.com/folke/which-key.nvim"
   }
 }
@@ -249,14 +249,14 @@ time([[Defining packer_plugins]], false)
 time([[Config for pretty-fold.nvim]], true)
 try_loadstring("\27LJ\2\na\0\0\3\0\4\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\0016\0\0\0'\2\3\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\24pretty-fold.preview\nsetup\16pretty-fold\frequire\0", "config", "pretty-fold.nvim")
 time([[Config for pretty-fold.nvim]], false)
--- Config for: whichkey
-time([[Config for whichkey]], true)
-try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\0", "config", "whichkey")
-time([[Config for whichkey]], false)
 -- Config for: vim-doge
 time([[Config for vim-doge]], true)
 try_loadstring("\27LJ\2\nA\0\0\2\0\4\0\0056\0\0\0009\0\1\0'\1\3\0=\1\2\0K\0\1\0\vgoogle\29doge_doc_standard_python\6g\bvim\0", "config", "vim-doge")
 time([[Config for vim-doge]], false)
+-- Config for: whichkey
+time([[Config for whichkey]], true)
+try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\0", "config", "whichkey")
+time([[Config for whichkey]], false)
 if should_profile then save_profiles() end
 
 end)
